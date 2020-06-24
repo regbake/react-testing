5d0114c31c8afb370d74edffd320ebc549d40d78
Reggie Baker <regbake@gmail.com>
2020-06-24 10:31:31 -0700

    add: reducers folder

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,4 +17,26 @@ it('has a text area and a button', () => {
 
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
-})
+});
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+
+    wrapped.update();
+  });
+
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('should clear the text area on submit', () => {
+    wrapped.find('form').simulate('submit');
+
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+});
