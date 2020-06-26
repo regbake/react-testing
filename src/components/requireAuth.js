30658699932a59126e3a48d15dc4d7c2be1b61f2
Reggie Baker <regbake@gmail.com>
2020-06-26 15:12:19 -0700

    add: higher order component

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,11 +1,32 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default (ChildComponent) => {
   class ComposedComponent extends Component {
+    // Component just rendered
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+
+    // Component just updated
+    componentDidUpdate() {
+      this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+      if (!this.props.auth) {
+        this.props.history.push('/');
+      }
+    }
+
     render() {
-      return <ChildComponent />;
+      return <ChildComponent {...this.props} />;
     }
   }
 
-  return ComposedComponent;
+  function mapStateToProps(state) {
+    return { auth: state.auth };
+  }
+
+  return connect(mapStateToProps)(ComposedComponent);
 };
